2d783a81ce45e31aae8336630ee1a7af374e54f5
Stefano <30950154+stefifox@users.noreply.github.com>
2021-06-07 23:26:21 +0200

    Added server connection

--- app/src/main/java/dev/stefifox/discordkeyboard/MainActivity.java
+++ app/src/main/java/dev/stefifox/discordkeyboard/MainActivity.java
@@ -3,12 +3,73 @@ package dev.stefifox.discordkeyboard;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static String url = "http://";
+    public static boolean statusC = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText ip = findViewById(R.id.ipinput);
+        final EditText port = findViewById(R.id.portinput);
+        final Button connect = findViewById(R.id.connectbutton);
+        final TextView status = findViewById(R.id.status);
+
+        status.setText("disconnected");
+        status.setTextColor(getColor(R.color.disconnect));
+
+        connect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                url = "http://" + ip.getText() + ":" + port.getText();
+                Toast.makeText(MainActivity.this, url, Toast.LENGTH_SHORT).show();
+                JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url + "/connect", null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONObject obj = new JSONObject(response.toString());
+                            Toast.makeText(MainActivity.this, obj.toString(), Toast.LENGTH_SHORT).show();
+                            statusC = true;
+                            status.setText("connected");
+                            status.setTextColor(getColor(R.color.connected));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        System.out.println(error);
+                        Toast.makeText(MainActivity.this,"Connection error", Toast.LENGTH_SHORT).show();
+                        statusC = false;
+                        url = "http://";
+                    }
+                });
+                MySingleton.getInstance(MainActivity.this).addToRequestQueue(request);
+            }
+        });
+
+        if(statusC){
+
+        }
+
     }
 }
