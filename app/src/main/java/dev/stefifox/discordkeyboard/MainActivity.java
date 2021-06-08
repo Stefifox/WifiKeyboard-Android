fbc6a1c6dd08032fdc13f30bc628511d6c3dc6a8
Stefano <30950154+stefifox@users.noreply.github.com>
2021-06-08 12:27:33 +0200

    Added get buttons from server

--- app/src/main/java/dev/stefifox/discordkeyboard/MainActivity.java
+++ app/src/main/java/dev/stefifox/discordkeyboard/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,6 +16,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -32,8 +34,7 @@ public class MainActivity extends AppCompatActivity {
         final EditText port = findViewById(R.id.portinput);
         final Button connect = findViewById(R.id.connectbutton);
         final TextView status = findViewById(R.id.status);
-
-        final ImageButton mic = findViewById(R.id.micbutton);
+        final LinearLayout buttonList = findViewById(R.id.buttonlist);
 
         status.setText("disconnected");
         status.setTextColor(getColor(R.color.disconnect));
@@ -48,14 +49,31 @@ public class MainActivity extends AppCompatActivity {
                     public void onResponse(JSONObject response) {
                         try {
                             JSONObject obj = new JSONObject(response.toString());
+                            JSONArray buttons = new JSONArray(obj.getJSONObject("configs").getJSONArray("buttons").toString());
+                            System.out.println(buttons.toString());
                             Toast.makeText(MainActivity.this, obj.toString(), Toast.LENGTH_SHORT).show();
                             statusC = true;
                             status.setText("connected");
                             status.setTextColor(getColor(R.color.connected));
+                            for(int i = 0; i < buttons.length(); i++){
+                                JSONObject temp = buttons.getJSONObject(i);
+                                Button btn = new Button(MainActivity.this);
+                                btn.setText(temp.getString("name"));
+                                btn.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        try {
+                                            request(String.valueOf(temp.getInt("id")));
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                });
+                                buttonList.addView(btn);
+                            }
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
-
                     }
                 }, new Response.ErrorListener() {
                     @Override
@@ -64,27 +82,20 @@ public class MainActivity extends AppCompatActivity {
                         Toast.makeText(MainActivity.this,"Connection error", Toast.LENGTH_SHORT).show();
                         statusC = false;
                         url = "http://";
+                        status.setText("disconnected");
+                        status.setTextColor(getColor(R.color.disconnect));
                     }
                 });
                 MySingleton.getInstance(MainActivity.this).addToRequestQueue(request);
             }
         });
 
-            mic.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if(statusC){
-                        request("mute");
-                    }
-
-                }
-            });
 
     }
 
-    private void request(String key){
+    private void request(String id){
         final TextView status = findViewById(R.id.status);
-        String reqUrl = url + "/key?value=" + key;
+        String reqUrl = url + "/key?id=" + id;
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, reqUrl, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
